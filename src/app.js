c3a490b7cbc8e2093117417390872fbc0fd00378
alejs-dev <alexmardarilel@gmail.com>
2025-03-23 12:59:16 +0100

    Added Routes for Skrem + DB + Modules

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@ import express, { Router } from "express";
 const app = express();
 const router = Router();
 
+let database = require("./src/db/skrem.js");
+let bodyParser = require("body-parser");
+
 app.use((request, response, next) => {
     response.setHeader("Kawasdasistdigga", "testdigga");
     next();
 });  
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 
 router.get("/", (request, response, next) => {
     return response.json({
@@ -15,6 +21,10 @@ router.get("/", (request, response, next) => {
     })
 });
 
+//-------------------------------------------------------------------------------------
+//------------------------- Routes for Memeliothek ------------------------------------
+//-------------------------------------------------------------------------------------
+
 router.get("/memeliothek", (request, response, next) => {
     return response.json({
         "message": "Memeliothek Home", 
@@ -22,7 +32,6 @@ router.get("/memeliothek", (request, response, next) => {
     })
 });
 
-// Routes for Azubis
 router.get("/memeliothek/azubis/alex", (request, response, next) => {
     response.json({
         "message": "success",
@@ -57,24 +66,124 @@ router.get("/memeliothek/azubis/patrick", (request, response, next) => {
         "data": "Patrick"
     });
 });
-// End of Routes for Azubis
 
-// Route for nah id win
 router.get("/memeliothek/nahidwin", (request, response, next) => {
     response.json({
         "message": "success",
         "data": "Nah id win"
     });
 });
-// End of Route for nah id win
 
-// Route for random pics
 router.get("/memeliothek/randompics", (request, response, next) => {
     response.json({
         "message": "success",
         "data": "Random pics"
     });
 });
-// End of Route for random pics 
 
+//-------------------------------------------------------------------------------------
+//------------------------- End Routes for Memeliothek --------------------------------
+//-------------------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------------------
+//------------------------- Routes for Skrem ------------------------------------------
+//-------------------------------------------------------------------------------------
+
+router.get("/skrem", (request, response, next) => {
+    let sql = "select * from skrem";
+    let params = [];
+    database.all(sql, params, (error, rows) => {
+        if (error) {
+            response.status(400).json({"error": error.message});
+            return;
+        };
+        response.json({
+            "message": "success",
+            "data": rows
+        });
+    });
+});
+
+router.get("/skrem/:id", (request, response, next) => {
+    let sql = "select * from skrem where id = ?";
+    let params = [request.params.id];
+    database.get(sql, params, (error, row) => {
+        if (error) {
+            response.status(400).json({"error": error.message});
+            return;
+        };
+        response.json({
+            "message": "success",
+            "data": row
+        });
+    });
+});
+
+router.post("/skrem", (request, response, next) => {
+    let errors = [];
+    if (!request.body.producer) {
+        errors.push("No producer specified");
+    }; 
+    if (!request.body.fname) {
+        errors.push("No fname specified");
+    }; 
+    let data = {
+        producer: request.body.producer,
+        fname: request.body.fname
+    }; 
+    let insert = 'INSERT INTO skrem (producer, fname) VALUES (?, ?)';
+    let params = [data.producer, data.fname];
+    database.run(insert, params, function(error) {
+        if (error) {
+            response.status(400).json({"error": error.message});
+            return;
+        } else {
+            response.json({
+                "message": "success",
+                "data": data,
+                "id": this.lastID
+            });
+        };
+    });
+});
+
+router.put("/skrem/:id", (request, response, next) => {
+    let data = {
+        producer: request.body.producer,
+        fname: request.body.fname
+    };
+    database.run(
+        `UPDATE skrem set 
+           producer = COALESCE(?, producer),
+           fname = COALESCE(?, fname)
+           WHERE id = ?`,
+        [data.producer, data.fname, request.params.id],
+        function(error) {
+            if (error) {
+                response.status(400).json({"error": response.message});
+                return;
+            } else {
+                response.json({
+                    message: "success",
+                    data: data
+                });
+            };
+        }
+    );
+});
+
+router.delete("/skrem/:id", (request, response, next) => {
+    database.run(
+        'DELETE FROM skrem WHERE id = ?',
+        request.params.id,
+        function(error) {
+            if (error) {
+                response.status(400).json({"error": response.message});
+                return;
+            } else {
+                response.json({"message": "deleted", rows: this.changes});
+            };
+        }
+    );
+});
 export { app, router };
---
+++ src/db/skrem.js
@@ -0,0 +1,10 @@
+const sql = require("sqlite3").verbose();
+const database = new sql.Database("./src/db/skrem.sqlite3");
+
+database.run(`CREATE TABLE IF NOT EXISTS skrem (
+    id INTEGER PRIMARY KEY,
+    producer TEXT, 
+    fname TEXT)`
+);
+
+module.exports = database;
