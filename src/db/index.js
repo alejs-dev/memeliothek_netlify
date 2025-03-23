79d935dcedf26b9239e28087a7f8761398bbd1ab
alejs-dev <alexmardarilel@gmail.com>
2025-03-23 15:23:38 +0100

    asdfasfd

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
 import sql from "sqlite3";
-const database = new sql.Database("../src/db/skrem.sqlite3");
+const database = new sql.Database("./src/db/skrem.sqlite3");
 
 database.run(`CREATE TABLE IF NOT EXISTS skrem (
     id INTEGER PRIMARY KEY,
