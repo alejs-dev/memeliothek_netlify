c52b1c56712ea43df379cc125494f00cfde5f103
alejs-dev <alexmardarilel@gmail.com>
2025-03-23 14:42:13 +0100

    Loads of structure Changed modules

--- src/app.js
+++ src/app.js
@@ -113,16 +113,18 @@ router.get("/skrem", (request, response, next) => {
 router.get("/skrem/:id", (request, response, next) => {
     let sql = "select * from skrem where id = ?";
     let params = [request.params.id];
-    database.get(sql, params, (error, row) => {
-        if (error) {
-            response.status(400).json({"error": error.message});
-            return;
-        };
-        response.json({
-            "message": "success",
-            "data": row
+    () => {
+        database.get(sql, params, (error, row) => {
+            if (error) {
+                response.status(400).json({"error": error.message});
+                return;
+            };
+            response.json({
+                "message": "success",
+                "data": row
+            });
         });
-    });
+    }
 });
 
 router.post("/skrem", (request, response, next) => {
--- src/db/skrem.js
+++ src/db/skrem.js
@@ -1,10 +1,10 @@
-import sql from "sqlite";
-const database = new sql.Database("./src/db/skrem.sqlite");
+import sql from "sqlite3";
+const database = new sql.Database("./src/db/skrem.sqlite3");
 
 database.run(`CREATE TABLE IF NOT EXISTS skrem (
     id INTEGER PRIMARY KEY,
     producer TEXT, 
-    fname TEXT)`
+    name TEXT)`
 );
 
 export { database };
